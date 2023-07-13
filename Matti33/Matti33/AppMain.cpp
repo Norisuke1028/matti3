d7c4aab46975a53be7ae2f440d98d07dd10a353a
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2023-07-13 12:11:41 +0900

    appmain制作中
    TitleScene_cpph.pdf終了

--- Matti33/Matti33/AppMain.cpp
+++ Matti33/Matti33/AppMain.cpp
@@ -1,23 +1,40 @@
+
+#include"TitleScene.h"
 #include"DxLib.h"
+#include"InputControl.h"
+#include"SceneManager.h"
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_
-LPSTR lpCmdLine,_In_ int nShowCmd)
+/**********************************
+*マクロ定義
+**********************************/
+
+/**********************************
+*型定義
+**********************************/
+
+/**********************************
+*変数宣言
+**********************************/
+
+int TitleImage;
+int TitleBGM;
+
+/*********************************
+*プロトタイプ宣言
+**********************************/
+
+/*********************************
+*タイトル画面：初期化処理
+*引　数：なし
+*戻り値：エラー情報
+**********************************/
+int TitleScene_Initialize(void)
 {
-	//ウィンドウモードで起動
-	ChangeWindowMode(TRUE);
+	int ret = 0;
 
-	//Dxライブラリの初期化処理
-	if (DxLib_Init() == -1)
-	{
-		return -1;
-	}
-
-	//入力待機
-	WaitKey();
-
-	//Dxライブラリ使用の終了処理
-	DxLib_End();
-
-	return 0;
-
+	//画像読み込み処理
+	TitleImage = LoadGraph("images/title.png");
+	//音源読み込み処理
+	TitleBGM = LoadSoundMem("sounds/title_bgm.mp3");
 }
+
